a163394c17fa0292234fb6b53a4032e4920d0aaa
Vihitha Ravindu <99358814+Vihitha-Ravindu@users.noreply.github.com>
2023-10-24 14:45:23 +0530

    service provider Authentication

--- app/src/main/java/com/example/taskbuddy/Modals/ServiceProviderModal.kt
+++ app/src/main/java/com/example/taskbuddy/Modals/ServiceProviderModal.kt
@@ -1,9 +1,12 @@
 package com.example.taskbuddy.Modals
 
-data class UserModal(
+data class ServiceProviderModal(
     var nic: String? = null,
     var name: String? = null,
     var email: String? = null,
     var number: String? = null,
-    var location: String? = null
+    var service: String? = null,
+    var location: String? = null,
+    var rating: Int? = null,
+    var status: String? = null
 )
